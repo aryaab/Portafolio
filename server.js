f60890cd43eafef9b902cec84429c82cb1c38873
Nidia Bustos <ndbustos@gmail.com>
2015-03-07 01:47:29 -0300

    modificopackage

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http'),
     st   = require('node-static'),
     opts = { cache: false },
-    file = new st.Server('./app', opts),
+    file = new st.Server('/', opts),
     port = process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
