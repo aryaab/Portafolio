807b0fd4b1797e019077d13e1029a5290b0d3387
Nidia Bustos <ndbustos@gmail.com>
2015-03-07 01:54:02 -0300

    test

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http'),
-    st   = require('node-static'),
-    opts = { cache: false },
-    file = new st.Server('/', opts),
-    port = process.env.PORT || 8080;
-
-http.createServer(function (req, res) {
-    file.serve(req, res);
-}).listen(port);
